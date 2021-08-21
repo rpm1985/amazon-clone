1a35db5235f82979f739dd33ab87fe32fa63b5a9
Richard <MordecaiRP@cardiff.ac.uk>
2021-08-21 19:14:34 +0100

    Added firebase Js code

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyC1XVqdA2nbPboOXQU3WRECM0G_BCIzaZ8",
+    authDomain: "clone-db711.firebaseapp.com",
+    projectId: "clone-db711",
+    storageBucket: "clone-db711.appspot.com",
+    messagingSenderId: "536673732689",
+    appId: "1:536673732689:web:5b3849595d19d4d798d070",
+    measurementId: "G-H5B865YX2L",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
